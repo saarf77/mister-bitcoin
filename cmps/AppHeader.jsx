557e09f3ef098a2bb4a7195fd23accbeab7bf027
saarf77 <saarf77@gmail.com>
2022-12-28 16:30:34 +0200

    mobile responisve and looks

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -7,16 +7,23 @@ function _AppHeader(props) {
       document.querySelector('.nav-wrapper nav').classList.toggle('open');
     };
     document.querySelector('.menu-button').addEventListener('click', toggleMenu);
+    document.querySelector('.nav-wrapper nav').addEventListener('click', toggleMenu);
+    document.addEventListener('click', event => {
+      if (!event.target.closest('.nav-wrapper')) {
+        toggleMenu();
+      }
+    });
     return () => {
       document.querySelector('.menu-button').removeEventListener('click', toggleMenu);
+      document.querySelector('.nav-wrapper nav').removeEventListener('click', toggleMenu);
+      document.removeEventListener('click', toggleMenu);
     };
   }, []);
-
   return (
     <header className="app-header">
       <h1>Mister-BITCoin</h1>
       <div className="nav-wrapper">
-        <button className="menu-button">Menu</button>
+        <button className="menu-button">☰</button>
         <nav>
           <NavLink exact to="/">
             Home
@@ -34,22 +41,3 @@ export const AppHeader = withRouter(_AppHeader);
 
 
 
-// import { NavLink, withRouter } from "react-router-dom";
-
-// function _AppHeader(props) {
-//   return (
-//     <header className="app-header">
-//       <h1>Mister-BITCoin</h1>
-//       <nav>
-//         <NavLink exact to="/">
-//           Home
-//         </NavLink>
-//         <NavLink to="/contact">Contacts</NavLink>
-//         <NavLink to="/statistics">Statistics</NavLink>
-//         <NavLink to="/signup">Signup</NavLink>
-//       </nav>
-//     </header>
-//   );
-// }
-
-// export const AppHeader = withRouter(_AppHeader);
